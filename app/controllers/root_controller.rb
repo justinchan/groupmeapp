2a31fdb25ef7088d5a935602c53b05840f49a64a
Justin Chan <hellojustinchan@gmail.com>
2013-06-18 10:24:20 -0700

    derp

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,7 +4,7 @@ def index
 	require 'net/http'
 	require 'json'
 	url = URI.parse('https://api.groupme.com/v3/bots/post')
-	post_args = {"bot_id" => 'a6541a77a0b7606b469f3bc3e2', "text" => "Did someone say HOT TUB?!"}.to_json
+	post_args = {"bot_id" => '36c92055df2c09e134144e1276', "text" => "Did someone say HOT TUB?!"}.to_json
 	a = ActiveSupport::JSON.decode(post_args)
 	text = params[:text].downcase
 	if params[:name] != 'Kanye'
