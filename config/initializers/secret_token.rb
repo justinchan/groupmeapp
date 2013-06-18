96440a252575c326d85aab4b39f84a4387ac9270
Justin Chan <hellojustinchan@gmail.com>
2013-06-17 21:53:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupme::Application.config.secret_token = 'f46a9109dafe5669b7bf13a7d6f5a89d0714321a5a0c6e1c6e7d877ee53e94286b2c00bd4f0f0ad6533bcab349a1d173a7e4befe3585044a42401efe2050cbcc'
