a65e8343baafb4377f7de5894990080e2d9a2689
Justin Chan <hellojustinchan@gmail.com>
2013-06-17 23:51:28 -0700

    derp

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,12 +2,13 @@ class RootController < ApplicationController
 
 def index
 	require 'net/http'
+	require 'json'
 	url = URI.parse('https://api.groupme.com/v3/bots/post')
-	post_args = {"fc1caa61bef8b652d64d3242d4" => params[:bot_id], "hello word" => params[:text]}
+	post_args = {"bot_id" => 'fc1caa61bef8b652d64d3242d4', "text" => "derp"}.to_json
+	a = ActiveSupport::JSON.decode(post_args)
 	#if params[:text] = 'hello'
-		resp, data = Net::HTTP.post_form(url, post_args)
+		resp, data = Net::HTTP.post_form(url, a)
 	#end
 end
 
 end
-
