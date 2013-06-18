3f680fc4cde512c6926e691b531666e3f80a08c5
Justin Chan <hellojustinchan@gmail.com>
2013-06-18 10:16:02 -0700

    changed kanye's picture

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,9 +4,9 @@ def index
 	require 'net/http'
 	require 'json'
 	url = URI.parse('https://api.groupme.com/v3/bots/post')
-	post_args = {"bot_id" => 'fc1caa61bef8b652d64d3242d4', "text" => "Did someone say HOT TUB?!"}.to_json
+	post_args = {"bot_id" => 'a6541a77a0b7606b469f3bc3e2', "text" => "Did someone say HOT TUB?!"}.to_json
 	a = ActiveSupport::JSON.decode(post_args)
-	text = params[:text]
+	text = params[:text].downcase
 	if params[:name] != 'Kanye'
 		if text["hot tub"] != nil
 			resp, data = Net::HTTP.post_form(url, a)
