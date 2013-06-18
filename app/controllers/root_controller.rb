a69768d3ef262c1f56f17727fab5a23c5ff7f0a6
Justin Chan <hellojustinchan@gmail.com>
2013-06-17 23:31:42 -0700

    derp

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,9 +4,9 @@ def index
 	require 'net/http'
 	url = URI.parse('https://api.groupme.com/v3/bots/post')
 	post_args = {'fc1caa61bef8b652d64d3242d4' => params[:bot_id], 'derp' => params[:text]}
-	if params[:text] = 'hello'
+	#if params[:text] = 'hello'
 		resp, data = Net::HTTP.post_form(url, post_args)
-	end
+	#end
 end
 
 end
