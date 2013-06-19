b746fadbd0ae1f342a87ac120da99e8bc9f50c59
Justin Chan <hellojustinchan@gmail.com>
2013-06-18 19:27:17 -0700

    extra strength algo

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,17 +1,19 @@
 class RootController < ApplicationController
 
-def index
-	require 'net/http'
-	require 'json'
-	url = URI.parse('https://api.groupme.com/v3/bots/post')
-	post_args = {"bot_id" => 'ef2a6aea6ec1d4d06d7727cbe9', "text" => "Did someone say HOT TUB?!"}.to_json
-	a = ActiveSupport::JSON.decode(post_args)
-	text = params[:text].downcase
-	if params[:name] != 'Kanye'
-		if text["hot tub"] != nil
-			resp, data = Net::HTTP.post_form(url, a)
+	def index
+		require 'net/http'
+		require 'json'
+		url = URI.parse('https://api.groupme.com/v3/bots/post')
+		post_args = {"bot_id" => 'ef2a6aea6ec1d4d06d7727cbe9', "text" => "Did someone say HOT TUB?!"}.to_json
+		a = ActiveSupport::JSON.decode(post_args)
+		text = params[:text].downcase
+		if params[:name] != 'Kanye'
+			if text["hot tub"] != nil
+				if text.split(" ").include?('hot') and text.split(" ").include?('tub')
+					resp, data = Net::HTTP.post_form(url, a)
+				end
+			end
 		end
 	end
-end
 
 end
