80846bffd9de41402124d7fe694b5b45b4964b14
Justin Chan <hellojustinchan@gmail.com>
2013-06-23 13:25:14 -0700

    derpy

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -31,6 +31,12 @@ class RootController < ApplicationController
 				url = URI.parse('https://api.groupme.com/v3/bots/post')
 				a = ActiveSupport::JSON.decode(post_args)
 				resp, data = Net::HTTP.post_form(url, a)
+			elsif text == 'kanye awards ceremony' and params[:name] == 'Justin Chan'
+				system "rake tabulate &"
+				url = URI.parse('https://api.groupme.com/v3/bots/post')
+				post_args = {"bot_id" => 'ef2a6aea6ec1d4d06d7727cbe9', "text" => "Tabulating results..."}.to_json
+				a = ActiveSupport::JSON.decode(post_args)
+				resp, data = Net::HTTP.post_form(url, a)
 			end
 		# 	elsif text["thanks kanye"] != nil
 		# 		first_name_temp = params[:name]
