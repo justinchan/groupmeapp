cb3c8622805e3236bd9a5c0dcffe2bac50fa0a82
Justin Chan <hellojustinchan@gmail.com>
2014-01-26 22:13:03 -0800

    small change

--- app/controllers/manual_controller.rb
+++ app/controllers/manual_controller.rb
@@ -7,7 +7,7 @@ class ManualController < ApplicationController
 		require 'json'
 		message = params[:kanye][:text]
 		url = URI.parse('https://api.groupme.com/v3/bots/post')
-		post_args = {"bot_id" => 'ef2a6aea6ec1d4d06d7727cbe9', "text" => "#{message}"}.to_json
+		post_args = {"bot_id" => '1ac7158d3e349fdd6174bc11e4', "text" => "#{message}"}.to_json
 		a = ActiveSupport::JSON.decode(post_args)
 		resp, data = Net::HTTP.post_form(url, a)
 		redirect_to manual_path
